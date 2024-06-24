489118571a6d847c1810a278bc7f132d94f026c5
indl1670 <indl1670@gmail.com>
2024-06-24 21:42:07 +0900

    [WEB] 레포 선택

--- src/components/common/ProjectSelectMenu.tsx
+++ src/components/common/ProjectSelectMenu.tsx
@@ -1,4 +1,5 @@
 import SendIcon from '@mui/icons-material/Send'
+import ChangeCircleOutlinedIcon from '@mui/icons-material/ChangeCircleOutlined'
 import { Input } from 'antd'
 import { useQuery } from 'react-query'
 import { getProjectList } from '../../apis/overview'
@@ -7,12 +8,15 @@ import { NavLink } from 'react-router-dom'
 import ControlPointIcon from '@mui/icons-material/ControlPoint'
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 import ProjectMenuList from '../base/common/ProjectMenuList'
-import { projectList } from '../../types/project'
+import { projectInfoSimple, projectList } from '../../types/project'
+import { useRecoilValue } from 'recoil'
+import { selectedProjectState } from '../../recoil/atoms/common'
 
 export default function ProjectSelectMenu() {
     const [isMenuShow, setIsMenuShow] = useState<boolean>(false)
     const [projectList, setProjectList] = useState<Array<projectList>>([])
-
+    const project = useRecoilValue<projectInfoSimple>(selectedProjectState)
+    
     const { data, isLoading } = useQuery({
         queryKey: ['getProjectList'],
         queryFn: () => getProjectList()
@@ -42,14 +46,23 @@ export default function ProjectSelectMenu() {
     if (!data || isLoading) return <></>
 
     return <div className='absolute ml-[-50px] mt-2'>
-        <div className='cursor-pointer px-3 text-white w-[200px] h-[40px] bg-black rounded-md font-light' onClick={() => setIsMenuShow(!isMenuShow)}>
-            <div className='flex flex-row justify-between'>
-                <p className='text-xl justify-center py-2'>Select Project</p>
-                <span className='py-2'><SendIcon/></span>
-            </div>
+        <div className='relative z-20 cursor-pointer px-3 text-white w-[200px] h-[40px] bg-black rounded-md font-light' onClick={() => setIsMenuShow(!isMenuShow)}>
+            {
+                project.projectName === '' ? (
+                    <div className='flex flex-row justify-between'>
+                        <p className='text-xl justify-center py-2'>Select Project</p>
+                        <span className='py-2'><SendIcon /></span>
+                    </div>
+                ) : (
+                    <div className='flex flex-row justify-between'>
+                        <p className='text-xl justify-center py-2'>{project.projectName}</p>
+                        <span className='py-2'><ChangeCircleOutlinedIcon /></span>
+                    </div>
+                )
+            }
         </div>
-        {isMenuShow && <div className='w-[200px] h-[300px] font-light bg-white relative z-10 mt-1 rounded-md p-2 shadow-lg'>
-            <Input placeholder='프로젝트명으로 검색' allowClear onChange={handleSearch}/>
+        {isMenuShow && <div className='w-[200px] h-[300px] font-light bg-white relative z-20 mt-1 rounded-md p-2 shadow-lg'>
+            <Input placeholder='🔎 프로젝트명으로 검색' allowClear onChange={handleSearch}/>
             <ProjectMenuList projects={projectList} />
             <div className='flex flex-row justify-between p-1 cursor-pointer mt-2 hover:bg-gray1'>
                 <NavLink to='/overview/select'>프로젝트 생성</NavLink>
---
+++ src/components/overview/repository/GitRepositoryList.tsx
@@ -0,0 +1,62 @@
+import { useQuery } from 'react-query'
+import { getGitRepositoryList } from '../../../apis/overview'
+import SpringImg from '../../../assets/images/overview/Spring.svg'
+import LockImg from '../../../assets/images/overview/Lock.svg'
+import { Button } from 'antd'
+import { useEffect, useState } from 'react'
+import { repositoryList } from '../../../types/project'
+
+export default function GitRepositoryList({...props}) {
+    const { orgName, searchText } = props
+    const [selectedRepo, setSelectedRepo] = useState<string>('')
+    const [repoList, setRepoList] = useState<Array<repositoryList>>([])
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['getGitRepositoryList', orgName],
+        queryFn: () => getGitRepositoryList(orgName, 10, 0)
+    })
+
+    const handleRepository = (repoName: string) => {
+        setSelectedRepo(repoName)
+    }
+
+    useEffect(() => {
+        const tmpRepoList: Array<repositoryList> = []
+
+        data?.forEach((item) => {
+            if (item.repoName.toLocaleLowerCase().replaceAll(' ', '').includes(searchText)) {
+                tmpRepoList.push(item)
+            }
+        })
+        setRepoList(tmpRepoList)
+    }, [data, searchText])
+
+    if (!data || isLoading) return <></>
+
+    return <div className='w-full h-full mt-2'> 
+        {
+            repoList.map((item, index) => (
+                <div className={`flex justify-between w-full p-2 border-[1px] border-gray1 ${index === 0 ? 'rounded-t-md' : index === data.length-1 ? 'rounded-b-md' : ''}`} key={index}>
+                    <div className='flex flex-row gap-3'>
+                        {
+                            item.projectType === 'spring' ? <img className='w-6' src={SpringImg} alt='스프링부트' /> : <div className='w-6 h-6 rounded-full border-[1px] border-gray1 mt-1'></div>
+                        }
+                        <p className='mt-1'>{item.repoName}</p>
+                        {
+                            item.isPrivate ? <img className='w-4' src={LockImg} alt="프라이빗" /> : <></>
+                        }
+                        <p className='mt-1 font-light text-gray10'>{item.updatedAt}</p>
+                    </div>
+                    <Button 
+                        type='primary'
+                        onClick={() => handleRepository(item.repoName)}
+                        disabled={selectedRepo === item.repoName}
+                        style={{width: 100}}
+                    >
+                        {selectedRepo === item.repoName ? 'Selected' : 'Select'}
+                    </Button>
+                </div>
+            ))
+        }
+    </div>
+}
