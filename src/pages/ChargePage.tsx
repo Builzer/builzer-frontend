a089e16a1bbc9badfc83d81f86037459f7752907
indl1670 <indl1670@gmail.com>
2024-07-11 22:07:33 +0900

    [WEB] 크레딧 자동 충전

--- src/pages/ChargePage.tsx
+++ src/pages/ChargePage.tsx
@@ -4,7 +4,13 @@ import CreditImg from "../assets/images/Credit.svg";
 import { useRecoilValue } from "recoil";
 import { userLoginInfo } from "../types/common";
 import { userInfoState } from "../recoil/atoms/common";
-import { Button, ConfigProvider, InputNumber, InputNumberProps } from "antd";
+import {
+  Button,
+  ConfigProvider,
+  InputNumber,
+  InputNumberProps,
+  message,
+} from "antd";
 import { useState } from "react";
 import { valueType } from "antd/es/statistic/utils";
 
@@ -12,11 +18,34 @@ export default function ChargePage() {
   const userInfo = useRecoilValue<userLoginInfo>(userInfoState);
   const [selectedAmount, setSelectedAmount] = useState<string>("5");
   const [etcValue, setEtcValue] = useState<valueType | null>();
+  const [selectedAutoBaseAmount, setSelectedAutoBaseAmount] =
+    useState<string>("10");
+  const [selectedAutoChargeAmount, setSelectedAutoChargeAmount] =
+    useState<string>("10");
+  const [autoBaseValue, setAutoBaseValue] = useState<valueType | null>();
+  const [autoChargeValue, setAutoChargeValue] = useState<valueType | null>();
 
   const creditAmount = ["5", "10", "20", "30", "40", "50", "기타"];
+  const autoCreditAmount = ["10", "20", "30", "기타"];
 
   const handleEtcValue: InputNumberProps["onChange"] = (value) => {
     setEtcValue(value);
+  };
+
+  const handleAutoBaseValue: InputNumberProps["onChange"] = (value) => {
+    setAutoBaseValue(value);
+  };
+
+  const handleAutoChargeValue: InputNumberProps["onChange"] = (value) => {
+    if (!!value) {
+      const chargeValue = Number(value.valueOf());
+      if (chargeValue <= Number(autoBaseValue)) {
+        message.warning("기준 크레딧보다 큰 금액을 입력해주세요.");
+        setAutoChargeValue(Number(autoBaseValue) + 1);
+      } else {
+        setAutoChargeValue(value);
+      }
+    }
   };
 
   return (
@@ -120,7 +149,150 @@ export default function ChargePage() {
                   </Button>
                 </div>
               </div>
-              <div className="w-1/2 h-[520px] border-[1px] border-gray1 bg-white p-2"></div>
+              <div className="w-1/2 h-[520px] border-[1px] border-gray1 bg-white p-2">
+                <p className="text-2xl font-bold">자동 충전</p>
+                <div className="mt-3 flex flex-row gap-2">
+                  <p>보유 크레딧</p>
+                  <img src={CreditImg} alt="크레딧 이미지" className="w-6" />
+                  <p className="font-bold">{userInfo.totalCredit}</p>
+                </div>
+                <div className="mt-3">
+                  <p>기준 크레딧</p>
+                  <div className="flex flex-row flex-wrap gap-3 mt-1">
+                    {autoCreditAmount.map((amount, index) => (
+                      <div
+                        key={index}
+                        className={`cursor-pointer flex flex-row gap-1 px-2 py-3 border-[1px] border-gray1 w-[103px] ${
+                          selectedAutoBaseAmount === amount
+                            ? "bg-black text-white"
+                            : ""
+                        }`}
+                        onClick={() => setSelectedAutoBaseAmount(amount)}
+                      >
+                        <img
+                          src={CreditImg}
+                          alt="크레딧 이미지"
+                          className="w-6"
+                        />
+                        {amount === "기타" ? (
+                          <ConfigProvider
+                            theme={{
+                              token: {
+                                colorPrimary:
+                                  selectedAutoBaseAmount === "기타"
+                                    ? "#000000"
+                                    : "#666666",
+                                colorBgBase:
+                                  selectedAutoBaseAmount === "기타"
+                                    ? "#000000"
+                                    : "#FFFFFF",
+                                colorTextPlaceholder:
+                                  selectedAutoBaseAmount === "기타"
+                                    ? "#999999"
+                                    : "#666666",
+                                colorTextBase:
+                                  selectedAutoBaseAmount === "기타"
+                                    ? "#FFFFFF"
+                                    : "#000000",
+                              },
+                            }}
+                          >
+                            <InputNumber
+                              placeholder={amount}
+                              value={autoBaseValue}
+                              size="small"
+                              className={`w-full ${
+                                selectedAutoBaseAmount === "기타"
+                                  ? "text-white"
+                                  : ""
+                              }`}
+                              onChange={handleAutoBaseValue}
+                            />
+                          </ConfigProvider>
+                        ) : (
+                          <p>{amount}</p>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <p>자동 충전 금액</p>
+                  <div className="flex flex-row flex-wrap gap-3 mt-1">
+                    {autoCreditAmount.map((amount, index) => (
+                      <div
+                        key={index}
+                        className={`cursor-pointer flex flex-row gap-1 px-2 py-3 border-[1px] border-gray1 w-[103px] ${
+                          selectedAutoChargeAmount === amount
+                            ? "bg-black text-white"
+                            : ""
+                        }`}
+                        onClick={() => setSelectedAutoChargeAmount(amount)}
+                      >
+                        <img
+                          src={CreditImg}
+                          alt="크레딧 이미지"
+                          className="w-6"
+                        />
+                        {amount === "기타" ? (
+                          <ConfigProvider
+                            theme={{
+                              token: {
+                                colorPrimary:
+                                  selectedAutoChargeAmount === "기타"
+                                    ? "#000000"
+                                    : "#666666",
+                                colorBgBase:
+                                  selectedAutoChargeAmount === "기타"
+                                    ? "#000000"
+                                    : "#FFFFFF",
+                                colorTextPlaceholder:
+                                  selectedAutoChargeAmount === "기타"
+                                    ? "#999999"
+                                    : "#666666",
+                                colorTextBase:
+                                  selectedAutoChargeAmount === "기타"
+                                    ? "#FFFFFF"
+                                    : "#000000",
+                              },
+                            }}
+                          >
+                            <InputNumber
+                              placeholder={amount}
+                              value={autoChargeValue}
+                              size="small"
+                              className={`w-full ${
+                                selectedAutoChargeAmount === "기타"
+                                  ? "text-white"
+                                  : ""
+                              }`}
+                              onChange={handleAutoChargeValue}
+                            />
+                          </ConfigProvider>
+                        ) : (
+                          <p>{amount}</p>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="mt-3">
+                  <p>결제방식</p>
+                  <div className="h-[135px]">결제방식 선택</div>
+                </div>
+                <div className="mt-5">
+                  <p className="font-thin text-sm text-center text-gray8">
+                    주문 내역을 확인했으며, 정보 제공에 동의합니다.
+                  </p>
+                  <Button
+                    type="primary"
+                    className="w-full bg-black mt-2"
+                    size="large"
+                  >
+                    결제하기
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
